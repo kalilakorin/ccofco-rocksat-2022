9d8c1ca4bf601053f98d66ef08406de2befe0c47
Theskyfalls <Skypuckett2302@gmail.com>
2022-01-03 16:24:59 -0700

    Moditfied distance test

--- distanceSensor.py
+++ distanceSensor.py
@@ -1,10 +1,11 @@
 ### Testings distance sensor
-
-import VL53l1X
+#import sys
+#sys.path.insert(0, "build/lib.linux-armv71-2.7/")
+import VL53L1X
 import time
 from datetime import datetime
 
-distance = VL53l1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
+distance = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
 distance.open()
 distance.start_ranging(1)
 try:
