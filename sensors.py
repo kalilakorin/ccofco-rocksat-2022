c4df89556171a3025f8448eac27059ffa2470571
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-01-03 17:31:23 -0700

    changes to sensors for distance sensor

--- sensors.py
+++ sensors.py
@@ -16,7 +16,7 @@ import busio
 
 import adafruit_mpl115a2
 from adafruit_bme280 import basic as adafruit_bme280
-import VL53L1X
+import adafruit_vl53l1x
 
 # Acquire the existing logger
 try:
@@ -52,9 +52,8 @@ def main():
         logging.error('Failed to enable BME280 (temperature, pressure, humidity) sensor')
     # Init vl53l1x distance sensor
     try:
-        vl53l1x = VL53L1X.VL53L1X(i2c)
-        vl53l1x.open()
-        vl53l1x.start_ranging(1)
+        vl53l1x = adafruit_vl53l1x.VL53L1X(i2c)
+        vl53l1x.start_ranging()
     except:
         vl53l1x = None
         logging.error('Failed to enable VL53L1X (distance) sensor')
@@ -83,7 +82,7 @@ def main():
         # Add entries to the CSV line based on the presence of those particular sensors
         if mpl115a2 != None: csvline += f',{mpl115a2.temperature},{mpl115a2.pressure}'
         if bme280 != None: csvline += f',{bme280.temperature},{bme280.pressure},{bme280.relative_humidity}'
-        if vl53l1x != None: csvline += f',{vl53l1x.get_distance()}'
+        if vl53l1x != None: csvline += f',{vl53l1x.distance}'
         
         datafile.write(csvline + '\n')
         # Print the CSV line to the console if the file is running standalone
