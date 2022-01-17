039734de3f1a2ec1e9c43968034735fa89f67d78
Theskyfalls <67851150+Theskyfalls@users.noreply.github.com>
2022-01-17 09:34:22 -0700

    Update Duel BME TEST.py

--- Duel BME TEST.py
+++ Duel BME TEST.py
@@ -8,8 +8,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 bme280a = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x77) #adress 0x77
 
-bme280b = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76) #adress 0x76
+#bme280b = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76) #adress 0x76
 
 for i in range(50):
     print(str(bme280a.temperature) + ' a')
-    print(str(bme280b.temperature) + ' b')
+    #print(str(bme280b.temperature) + ' b')
