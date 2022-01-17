e59626d65b9c3313983716c5609565d731d32a4f
Theskyfalls <67851150+Theskyfalls@users.noreply.github.com>
2022-01-17 13:35:16 -0700

    testing gpio

--- gpio pin test.py
+++ gpio pin test.py
@@ -9,27 +9,49 @@ GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Extension Limit Switch
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch
 
+# while True:
+#     if GPIO.input(13):
+#         print("TE-1: " + str(GPIO.input(13)))
+#
+#     else:
+#         print("...")
+#
+#     if GPIO.input(15):
+#         print("Extension Limit Switch detected")
+#     else:
+#         print("...")
+#     if GPIO.input(16):
+#         print("TE-2 detected")
+#     else:
+#         print("...")
+#
+#     if GPIO.input(18):
+#         print("Retraction Limit Switch detected")
+#     else:
+#         print("...")
+#     sleep(3)
 
+# wait for TE-1 signal
 
+while True :
+    if GPIO.input (13) :
+        break
 
-while True:
-    if GPIO.input(13):
-        print("TE-1: " + str(GPIO.input(13)))
+print ("TE-1 Detected...\n\n")
+# wait for extension limit switch activation
+while True :
+    if GPIO.input (15) :
+        break
+print ("Extension Stop Detected...\n\n")
+# wait for TE-2 signal
+while True :
+    if GPIO.input (16) :
+        break
+print ("TE-2 Detected...\n\n")
+# wait for retraction limit switch activation
+while True :
+    if GPIO.input (18) :
+        break
+print ("Retraction Stop Detected...\n\n")
 
-    else:
-        print("...")
-
-    if GPIO.input(15):
-        print("Extension Limit Switch detected")
-    else:
-        print("...")
-    if GPIO.input(16):
-        print("TE-2 detected")
-    else:
-        print("...")
-
-    if GPIO.input(18):
-        print("Retraction Limit Switch detected")
-    else:
-        print("...")
-    sleep(3)
+GPIO.cleanup ()
