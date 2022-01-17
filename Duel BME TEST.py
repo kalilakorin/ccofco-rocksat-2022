45f0b2f96560798e84fa4368e47921c93cffc781
Theskyfalls <67851150+Theskyfalls@users.noreply.github.com>
2022-01-17 09:22:00 -0700

    Update Duel BME TEST.py

--- Duel BME TEST.py
+++ Duel BME TEST.py
@@ -7,10 +7,10 @@ from adafruit_bme280 import basic as adafruit_bme280
 i2c = busio.I2C(board.SCL, board.SDA)
 
 bme280a = adafruit_bme280.Adafruit_BME280_I2C(i2c) #adress 0x77
-bme280.sea_level_pressure = 1013.25
+bme280a.sea_level_pressure = 1013.25
 
 bme280b = adafruit_bme280.Adafruit_BME280_I2C(i2c) #adress 0x77
-bme280.sea_level_pressure = 1013.25
+bme280b.sea_level_pressure = 1013.25
 
 
 for i in range(50):
