265322a1e44ec5d193f93805807435acc7a9d56f
Theskyfalls <67851150+Theskyfalls@users.noreply.github.com>
2022-01-17 09:33:28 -0700

    addressing change

--- Duel BME TEST.py
+++ Duel BME TEST.py
@@ -6,12 +6,9 @@ from adafruit_bme280 import basic as adafruit_bme280
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
-bme280a = adafruit_bme280.Adafruit_BME280_I2C(i2c) #adress 0x77
-bme280a.sea_level_pressure = 1013.25
+bme280a = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x77) #adress 0x77
 
-bme280b = adafruit_bme280.Adafruit_BME280_I2C(i2c) #adress 0x77
-bme280b.sea_level_pressure = 1013.25
-
+bme280b = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76) #adress 0x76
 
 for i in range(50):
     print(str(bme280a.temperature) + ' a')
