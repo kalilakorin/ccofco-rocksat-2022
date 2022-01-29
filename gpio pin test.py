721cfbe7e09a5e7fb40db7433a0672601d3cacde
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-01-28 18:28:51 -0700

    Update gpio pin test.py

--- gpio pin test.py
+++ gpio pin test.py
@@ -6,12 +6,17 @@ import time
 from adafruit_motorkit import MotorKit
 
 motor = MotorKit()
+pin1 = 27
+pin2 = 22
+pin3 = 23
+pin4 = 24
 
-GPIO.setmode(GPIO.BOARD)  # Pin# not GPIO#
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-1 around +85 seconds  GPIO27 blue
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Extension Limit Switch   GPIO22 yellow
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds GPIO23 blue
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch  GPIO24 yellow
+
+GPIO.setmode(GPIO.BCM)  # Pin# not GPIO#
+GPIO.setup(pin1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-1 around +85 seconds  GPIO27 blue
+GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Extension Limit Switch   GPIO22 yellow
+GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds GPIO23 blue
+GPIO.setup(pin4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch  GPIO24 yellow
 
 # while True:
 #     if GPIO.input(13):
@@ -38,26 +43,26 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch  
 # wait for TE-1 signal
 
 while True :
-    if GPIO.input (13) :
+    if GPIO.input (pin1) :
         break
 
 motor.motor1.throttle = 1
 print ("TE-1 Detected...\n\n")
 # wait for extension limit switch activation
 while True :
-    if GPIO.input (15) :
+    if GPIO.input (pin2) :
         break
 motor.motor1.throttle = 0
 print ("Extension Stop Detected...\n\n")
 # wait for TE-2 signal
 while True :
-    if GPIO.input (16) :
+    if GPIO.input (pin3) :
         break
 motor.motor1.throttle = -0.5
 print ("TE-2 Detected...\n\n")
 # wait for retraction limit switch activation
 while True :
-    if GPIO.input (18) :
+    if GPIO.input (pin4) :
         break
 motor.motor1.throttle = 0
 
