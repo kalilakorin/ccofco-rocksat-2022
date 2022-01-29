40c7ecf0cbe5191ee4e83fefa68857bf9e8ebb57
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-01-28 17:25:54 -0700

    Update motor test.py

--- motor test.py
+++ motor test.py
@@ -6,9 +6,12 @@ from adafruit_motorkit import MotorKit
 motor = MotorKit()
 
 motor.motor1.throttle = 0.5
-time.sleep(5)
+time.sleep(5000)
 motor.motor1.throttle = 0
-time.sleep(2)
+time.sleep(2000)
 motor.motor1.throttle = -0.5
-time.sleep(5)
+time.sleep(5000)
+motor.motor1.throttle = 0
+
+
 
