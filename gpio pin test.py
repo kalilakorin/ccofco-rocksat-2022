623f6d6c67619fed283c444953bcae49a72b824e
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-01-28 17:49:19 -0700

    Update gpio pin test.py

--- gpio pin test.py
+++ gpio pin test.py
@@ -2,6 +2,10 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
+import time
+from adafruit_motorkit import MotorKit
+
+motor = MotorKit()
 
 GPIO.setmode(GPIO.BOARD)  # Pin# not GPIO#
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-1 around +85 seconds
@@ -37,25 +41,25 @@ while True :
     if GPIO.input (13) :
         break
 
-
+motor.motor1.throttle = 1
 print ("TE-1 Detected...\n\n")
 # wait for extension limit switch activation
 while True :
     if GPIO.input (15) :
         break
-
+motor.motor1.throttle = 0
 print ("Extension Stop Detected...\n\n")
 # wait for TE-2 signal
 while True :
     if GPIO.input (16) :
         break
-
+motor.motor1.throttle = -0.5
 print ("TE-2 Detected...\n\n")
 # wait for retraction limit switch activation
 while True :
     if GPIO.input (18) :
         break
-
+motor.motor1.throttle = 0
 
 
 GPIO.cleanup ()
