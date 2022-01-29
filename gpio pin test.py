a6d09c79bf41af826050076b83765eb839814372
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-01-28 18:20:05 -0700

    comments

--- gpio pin test.py
+++ gpio pin test.py
@@ -8,10 +8,10 @@ from adafruit_motorkit import MotorKit
 motor = MotorKit()
 
 #GPIO.setmode(GPIO.BOARD)  # Pin# not GPIO#
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-1 around +85 seconds
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Extension Limit Switch
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-1 around +85 seconds  GPIO27 blue
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Extension Limit Switch   GPIO22 yellow
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds GPIO23 blue
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch  GPIO24 yellow
 
 # while True:
 #     if GPIO.input(13):
