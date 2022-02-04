c3fc5ee539b2f75390ff75687208e216283b33f5
kalilakorin <kalila.korin@gmail.com>
2022-02-04 12:37:56 -0700

    add test file for starting the gopro

--- startgopro.py
+++ startgopro.py
@@ -1,5 +1,33 @@
 #command that can be used to start the gopro
 
-import gopromain
+import gopromain as gopro
+import time
 
-gopromain.py --verbose --address "D1:70:A4:FC:21:4F" --command "record start"
+#--verbose --address "D1:70:A4:FC:21:4F" --command "record start"
+
+gopro.is_verbose = True
+#set the address
+gopro.address = "D1:70:A4:FC:21:4F"
+
+print(gopro.settings_supported.values())
+
+'''
+#try to activate and connect to the gopro
+goproIsOff = True
+if (goproIsOff):
+    try:
+        time.sleep(1)
+        gopro.run()
+    except:
+        goproIsOff = False
+        #continue
+
+#set the resolution
+
+#set the video mode / 360
+
+#set the frame rate
+
+#start the video recording    
+gopro.commands_supported = "record start"
+'''
