fc0c7d1bc200544346b867ba09ba7eb6de059554
kalilakorin <kalila.korin@gmail.com>
2022-02-04 10:31:07 -0700

    update to include gopro connections installs

---
+++ gopro.py
@@ -0,0 +1,11 @@
+'''
+    This file handles the start of recording for the camera
+    Contributors:
+        Jillian Frimml
+        Skyler Puckett
+        Konstantin Zaremski
+'''
+
+#import modules
+import gopro-ble-py-2
+
