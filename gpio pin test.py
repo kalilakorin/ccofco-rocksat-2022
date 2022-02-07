46114490fcd786b9985f4b5b76b171fd44873f6e
Theskyfalls <67851150+Theskyfalls@users.noreply.github.com>
2022-02-06 20:26:05 -0700

    Update gpio pin test.py

--- gpio pin test.py
+++ gpio pin test.py
@@ -18,33 +18,9 @@ GPIO.setup(pin2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Extension Limit Switch 
 GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds GPIO23 blue
 GPIO.setup(pin4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch  GPIO24 yellow
 
-# while True:
-#     if GPIO.input(13):
-#         print("TE-1: " + str(GPIO.input(13)))
-#
-#     else:
-#         print("...")
-#
-#     if GPIO.input(15):
-#         print("Extension Limit Switch detected")
-#     else:
-#         print("...")
-#     if GPIO.input(16):
-#         print("TE-2 detected")
-#     else:
-#         print("...")
-#
-#     if GPIO.input(18):
-#         print("Retraction Limit Switch detected")
-#     else:
-#         print("...")
-#     sleep(3)
-
 # wait for TE-1 signal
-
-
-
 while True :
+    print("...")
     if GPIO.input (pin1):
         break
 
