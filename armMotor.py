9d617115fc495587c770712005c4066537f7fa8a
Theskyfalls <67851150+Theskyfalls@users.noreply.github.com>
2022-02-06 18:41:34 -0700

    logger updates

--- armMotor.py
+++ armMotor.py
@@ -30,14 +30,20 @@ def main():
     # Configure & initialize the motor hat and GPIO pins
     logging.info('Initializing motor hat')
 
+    te1 = 27 # TE-1
+    lse = 22 # Limit Switch Extension
+    te2 = 23 # TE-2
+    lsr = 24 # Limit Switch Retraction
+
     # GPIO pin assignment
     try:
-        GPIO.setmode (GPIO.BOARD)  # Pin# not GPIO#
-        GPIO.setup (13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-1 around +85 seconds
-        GPIO.setup (15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Extension Limit Switch
-        GPIO.setup (16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds
-        GPIO.setup (18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch
         motor = MotorKit()
+        GPIO.setmode (GPIO.BMC)  #GPIO PIN NAMES
+        GPIO.setup (te1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-1 around +85 seconds
+        GPIO.setup (lse, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Extension Limit Switch
+        GPIO.setup (te2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds
+        GPIO.setup (lsr, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Retraction Limit Switch
+
     except:
         logger.critical('Failed to initialize GPIO pins and motor hat ')
         return
@@ -46,28 +52,28 @@ def main():
 
     # wait for TE-1 signal
     while True:
-        if GPIO.input(13):
+        if GPIO.input(te1):
             break
     logger.info ('TE-1 detected: ' + str (int (time.time () * 1000)))
     # set throttle (extension)
     motor.motor1.throttle = 1.0
     # wait for extension limit switch activation
     while True:
-        if GPIO.input(15):
+        if GPIO.input(lse):
             break
     logger.info ('Extension stop detected: ' + str (int (time.time () * 1000)))
     # set throttle (stop)
     motor.motor1.throttle = 0
     # wait for TE-2 signal
     while True:
-        if GPIO.input(16):
+        if GPIO.input(te2):
             break
     logger.info ('TE-2 detected: ' + str (int (time.time () * 1000)))
     # set throttle (retraction)
     motor.motor1.throttle = -1.0
     # wait for retraction limit switch activation
     while True:
-        if GPIO.input(18):
+        if GPIO.input(lsr):
             break
     logger.info ('Retraction stop detected: ' + str (int (time.time () * 1000)))
     # set throttle (stop)
