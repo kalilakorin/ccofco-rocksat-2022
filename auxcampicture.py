748fe84b6770cdf9297a4dc0c5c977f254718ca3
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-02-25 19:41:57 -0700

    Create auxcampicture.py

---
+++ auxcampicture.py
@@ -0,0 +1,8 @@
+import time
+import subprocess
+
+
+# ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 out.jpg
+for i in range(0,69):
+
+    subprocess.call(f'ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 ./pics/Pic{str(i)}{str(time.time())}.jpg', shell=True)
