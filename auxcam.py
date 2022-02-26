70aad4bd502cbcf0b322ff64b81abecdc9febf07
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-02-25 18:47:23 -0700

    Update auxcam.py

--- auxcam.py
+++ auxcam.py
@@ -5,6 +5,7 @@
 
 import subprocess
 import RPi.GPIO as GPIO
+import time
 
 pin3 = 23
 GPIO.setmode(GPIO.BCM)
@@ -12,7 +13,9 @@ GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 second
 
 subprocess.call('ffmpeg -t "01:00" -y -video_size 1280x720 -framerate 60 -i /dev/video0 -f flv test.flv', shell=True)
 
-while True :
-    if GPIO.input (pin3):
-        break
+# while True:
+#     if GPIO.input(pin3):
+#         break
+
+sleep(10)
 subprocess.call('signal.SIGINT', shell=True)
