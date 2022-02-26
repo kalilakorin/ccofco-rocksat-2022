4d2c6ce932f7441fb8c1a81b940136d65c660cfa
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-02-25 17:57:52 -0700

    for jill

--- auxcam.py
+++ auxcam.py
@@ -5,5 +5,5 @@
 
 import subprocess
 
-subprocess.call('ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 out.jpg', shell=True)
+subprocess.call('ffmpeg -y -video_size 1280x720 -framerate 60 -i /dev/video0 -f flv test.flv', shell=True)
 
