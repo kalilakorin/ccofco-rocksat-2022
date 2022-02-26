ca9eb673545b85e908dcdc8e41a358dae0f09356
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-02-25 17:39:56 -0700

    Update auxcam.py

--- auxcam.py
+++ auxcam.py
@@ -1,8 +1,9 @@
 # Skyler Puckett
 #
 # ffmpeg -y -video_size 1280x720 -framerate 60 -i /dev/video0 -f flv test.flv
+# ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 out.jpg
 
 import subprocess
 
-subprocess.call('ffmpeg -y -video_size 1280x720 -framerate 60 -i /dev/video0 -f flv test.flv', shell=True)
+subprocess.call('ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 out.jpg', shell=True)
 
