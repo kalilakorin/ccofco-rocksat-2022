148769d07cdf8528208f73440a443df243b91c38
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-02-25 20:04:03 -0700

    Update auxcampicture.py

--- auxcampicture.py
+++ auxcampicture.py
@@ -6,5 +6,7 @@ subprocess.call('mkdir -p pics', shell=True)
 i = 0
 while True:
 
-    subprocess.call(f'ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 ./pics/pic{str(i)}_{str(time.time())}.jpg ; sync', shell=True)
+    # subprocess.call(f'ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -frames 1 ./pics/pic{str(i)}_{str(time.time())}.jpg ; sync', shell=True)
+
+    subprocess.call(f'ffmpeg ffmpeg -i - -c:v mjpeg -f mjpeg ./pics/pic{str(i)}_{str(time.time())}.jpg ; sync', shell=True)
     i += 1
