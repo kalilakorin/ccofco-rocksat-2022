72f093e5d0e4180dddcb91ab4494cbc374e56bab
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-02-25 19:20:08 -0700

    Update auxcam.py

--- auxcam.py
+++ auxcam.py
@@ -14,7 +14,7 @@ def main():
     # GPIO.setmode(GPIO.BCM)
     # GPIO.setup(pin3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-2 around +220 seconds GPIO23 blue
 
-    subprocess.call(['', 'ffmpeg -t "01:00" -y -video_size 1280x720 -framerate 60 -i /dev/video0 -f flv test.flv'])
+    subprocess.call(['ffmpeg', '-t "01:00" -y -video_size 1280x720 -framerate 60 -i /dev/video0 -f flv test.flv'])
 
     # subprocess.call('ffmpeg -t "07:25" -y -video_size 1280x720 -framerate 60 -i /dev/video0 -f flv test.flv', shell=True)
     # print("stop soon")
