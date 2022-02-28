17441cc0be21f9603afa74ec9046e32c74c3076b
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2022-02-27 21:57:56 -0700

    OVERHAUL SENSORS

--- sensors.py
+++ sensors.py
@@ -31,6 +31,35 @@ except:
 
 # Main sensor program loop
 def main():
+    # Sensor information and init functions
+    sensors = {
+        'MPL115A2': {
+            addr: 0x60
+            type: 'temperature, pressure'
+            init: adafruit_mpl115a2.MPL115A2
+        }
+        'BME280A': {
+            addr: 0x77,
+            type: 'outside temperature, pressure, humidity'
+            init: adafruit_bme280.Adafruit_BME280_I2C
+        }
+        'BME280B': {
+            addr: 0x76,
+            type: 'inside temperature, pressure, humidity'
+            init: adafruit_bme280.Adafruit_BME280_I2C
+        }
+        'VL53L1X': {
+            addr: 0x29
+            type: 'distance',
+            init: adafruit_vl53l1x.VL53L1X
+        }
+        'ADXL34X': {
+            addr: 0x53,
+            type: 'acceleration',
+            init: adafruit_adxl34x.ADXL345
+        }
+    }
+
     # Configure & initialize the sensors
     logging.info('Initializing sensors')
 
@@ -41,40 +70,32 @@ def main():
     except: 
         logging.critical('Failed to enable i2c interface, the sensor thread will now crash')
         return
-    # Init mpl sensor
-    try:
-        mpl115a2 = adafruit_mpl115a2.MPL115A2(i2c)
-        logging.info('MPL115A2 (temperature, pressure) ... OK')
-    except: 
-        mpl115a2 = None
-        logging.error('Failed to enable MPL115A2 sensor')
-    # Init bme280 Outside
-    try:
-        bme280a = adafruit_bme280.Adafruit_BME280_I2C(i2c) #address 0x77 DEFAULT (outside)
-    except:
-        bme280a = None
-        logging.error('Failed to enable "outside" BME280 (temperature, pressure, humidity) sensor')
-    # Init bme280 Inside
-    try:
-        bme280b = adafruit_bme280.Adafruit_BME280_I2C(i2c, 0x76) #address 0x76 ALTERNATIVE (inside EBox)
-    except:
-        bme280b = None
-        logging.error('Failed to enable BME280 (temperature, pressure, humidity) sensor')
-    # Init vl53l1x distance sensor
-    try:
-        vl53l1x = adafruit_vl53l1x.VL53L1X(i2c)
-        vl53l1x.start_ranging()
-    except:
-        vl53l1x = None
-        logging.error('Failed to enable VL53L1X (distance) sensor')
-    # ADXL34x accelerometer
-    try:
-        adxl34x = adafruit_adxl34x.ADXL345(i2c)
-    except:
-        adxl34x = None
-        logging.error('Failed to enable ADXL34X (accelerometer) sensor')
 
-    logging.info('Sensors initialized')
+    # Query all the devices on the i2c bus
+    i2cDevices = i2c.scan()
+
+    # Sensors
+    mpl115a2 = None
+    bme280a = None
+    bme280b = None
+    vl53l1x = None
+    adxl34x = None
+
+    # For each sensor that we have defined
+    for name, sensor in sensors:
+        # If the address of the sensor is not in the i2c bus, we can skip it
+        if sensor.addr not in i2cDevices:
+            logging.info(f'{name} ({sensor.type}) was not detected on the i2c bus')
+            continue
+        # Attempt to initialize the sensor
+        try:
+            # Dynamic assignment to the sensor's variable using the init function defined in the sensors object
+            globals()[name.lower()] = sensor.init(i2c, sensor.addr)
+            logging.info(f'{name} ({sensor.type}) ... OK')
+        except: 
+            logging.error(f'Failed to enable {name} ({sensor.type}) sensor')
+
+    logging.info('Sensor initialization complete')
 
     # Create the output directory if it does not exist yet
     os.system('mkdir -p ./data-sensors')
@@ -86,16 +107,15 @@ def main():
     # CSV header line
     csvheader = 'Time'
     if mpl115a2 != None: csvheader += ',MPL115A2 Temperature, MPL115A2 Pressure'
-    if vl53l1x != None: csvheader += ',vl53l1x Distance'
+    if vl53l1x != None: csvheader += ',VL53L1X Distance'
     if bme280a != None: csvheader += ',Outside BME280 Temperature, Outside BME280 Pressure, Outside BME280 Humidity'
-    if bme280b != None : csvheader += ',Inside BME280 Temperature, Inside BME280 Pressure, Inside BME280 Humidity'
-    if adxl34x != None: csvheader += ',adxl34x Accelerometer X-axis, adxl34x Accelerometer Y-axis, adxl34x Accelerometer Z-axis'
-
+    if bme280b != None: csvheader += ',Inside BME280 Temperature, Inside BME280 Pressure, Inside BME280 Humidity'
+    if adxl34x != None: csvheader += ',ADXL34X Accelerometer X-axis, ADXL34X Accelerometer Y-axis, ADXL34X Accelerometer Z-axis'
     datafile.write(csvheader + '\n')
     logging.info(f'Sensor file CSV columns are as follows: {csvheader}')
 
     # Sensor sample and data write loop
-    logging.info('Beginning sensor polling and writing (1000 samples/second)')
+    logging.info('Beginning sensor polling and writing')
     while True:
         # Time axis
         csvline = str(int(time.time() * 1000))
@@ -104,13 +124,12 @@ def main():
         if mpl115a2 != None: csvline += f',{mpl115a2.temperature},{mpl115a2.pressure}'
         if vl53l1x != None: csvline += f',{vl53l1x.distance}'
         if bme280a != None: csvline += f',{bme280a.temperature},{bme280a.pressure},{bme280a.relative_humidity}'
-        if bme280b != None : csvline += f',{bme280b.temperature},{bme280b.pressure},{bme280b.relative_humidity}'
-        if adxl34x != None : csvline += f',{adxl34x.acceleration[0]},{adxl34x.acceleration[1]},{adxl34x.acceleration[2]}'
+        if bme280b != None: csvline += f',{bme280b.temperature},{bme280b.pressure},{bme280b.relative_humidity}'
+        if adxl34x != None: csvline += f',{adxl34x.acceleration[0]},{adxl34x.acceleration[1]},{adxl34x.acceleration[2]}'
         
         datafile.write(csvline + '\n')
         # Print the CSV line to the console if the file is running standalone
         if logger == None: print(csvline)
-        sleep(0.001)
 
 if __name__ == '__main__':
     main()
