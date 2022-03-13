580ef5694ff06d94a12648e32b1eefa45b32e2d5
kalilakorin <kalila.korin@gmail.com>
2022-03-13 16:11:06 -0600

    update start test script

--- startgopro.py
+++ startgopro.py
@@ -48,10 +48,10 @@ gopro.commands_supported = "record start"
 print('Setting default video')
 gopro.settings_supported.setdefault("r5")
 
-subprocess.call(f'python3 gopromain.py --verbose --address "D1:70:A4:FC:21:4F" --command "preset maxvideo" --command "record start"', shell=True)
+subprocess.call(f'python3 gopromain.py --verbose --address "D1:70:A4:FC:21:4F" --command "record start"', shell=True)
 
 time.sleep(5)
-subprocess.call(f'python3 gopromain.py --verbose --address "D1:70:A4:FC:21:4F" --command "preset maxvideo" --command "record stop"', shell=True)
+subprocess.call(f'python3 gopromain.py --verbose --address "D1:70:A4:FC:21:4F" --command "record stop"', shell=True)
 
 '''
 from goprocam import GoProCamera, constants
