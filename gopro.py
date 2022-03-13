8a2bf1c8736cdda36c6bceae4b3d8fb8180b8e91
kalilakorin <kalila.korin@gmail.com>
2022-03-13 15:29:01 -0600

    update test script

--- gopro.py
+++ gopro.py
@@ -1,5 +1,13 @@
 from open_gopro import GoPro
 
-with GoPro() as gopro:
-    print("Yay! I'm connected via BLE, Wifi, initialized, and ready to send / get data now!")
+import gopromain as gopro
+import time
+import subprocess
+
+subprocess.call(f'python3 gopromain.py --verbose --address "D1:70:A4:FC:21:4F" --command "preset maxvideo" --command "record start" ; sync', shell=True)
+
+
+#with GoPro() as gopro:
+#    print("Yay! I'm connected via BLE, Wifi, initialized, and ready to send / get data now!")
     # Send some commands now
+
