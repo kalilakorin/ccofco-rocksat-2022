a4bb0a6787e4f2c93ed7d6de8be225d08d397e0a
kalilakorin <kalila.korin@gmail.com>
2022-03-13 16:23:15 -0600

    update start test script

--- startgopro.py
+++ startgopro.py
@@ -2,15 +2,17 @@
 #https://gopro.github.io/labs/control/settings/
 #resolution "r5" is 5k360 for the max r5K or "24"
 
-import gopromain as gopro
+#import gopromain as gopro
 import time
 import subprocess
+from open_gopro import GoPro
 
 
-gopro.is_verbose = True
+#gopro.is_verbose = True
 #set the address
-gopro.address = "D1:70:A4:FC:21:4F"
-print('address:', gopro.address)
+
+#gopro.address = "D1:70:A4:FC:21:4F"
+#print('address:', gopro.address)
 
 '''
 for key, value in gopro.settings_supported.items():
@@ -44,6 +46,7 @@ if (goproIsOff):
 gopro.commands_supported = "record start"
 '''
 
+'''
 #resolution "r5" is 5k360 for the max
 print('Setting default video')
 gopro.settings_supported.setdefault("r5")
@@ -52,6 +55,7 @@ subprocess.call(f'python3 gopromain.py --verbose --address "D1:70:A4:FC:21:4F" -
 subprocess.call(f'python3 gopromain.py --verbose --address "D1:70:A4:FC:21:4F" --command "record start"', shell=True)
 time.sleep(5)
 subprocess.call(f'python3 gopromain.py --verbose --address "D1:70:A4:FC:21:4F" --command "record stop"', shell=True)
+'''
 
 '''
 from goprocam import GoProCamera, constants
@@ -60,3 +64,8 @@ goproCamera = GoProCamera.GoPro()
 
 goproCamera.shoot_video(10)
 '''
+
+with GoPro() as gopro:
+    print("Yay! I'm connected via BLE, Wifi, initialized, and ready to send / get data now!")
+    gopro.ble_setting.resolution.get_value()
+    # Send some commands now
