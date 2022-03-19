07986a52b09dbac2b2fd315bac230a0be4dde6f3
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-03-18 18:09:22 -0600

    tab button bud

--- gopro.py
+++ gopro.py
@@ -9,15 +9,15 @@ GPIO.setmode(GPIO.BCM)  #GPIO PIN NAMES
 ter = 17
 GPIO.setup(ter, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # TE-1 around +85 seconds
 
-# def main():
-#     while True:
-#         if GPIO.input(ter):
-#             break
-subprocess.call(f'python3 gopromain.py --verbose -a "D1:70:A4:FC:21:4F" -c "preset maxvideo" -c "record start"', shell=True)
+def main():
+    while True:
+        if GPIO.input(ter):
+            break
+    subprocess.call(f'python3 gopromain.py --verbose -a "D1:70:A4:FC:21:4F" -c "preset maxvideo" -c "record start"', shell=True)
 
 
-# if __name__ == '__main__':
-#     main()
+ if __name__ == '__main__':
+     main()
 
 #with GoPro() as gopro:
 #    print("Yay! I'm connected via BLE, Wifi, initialized, and ready to send / get data now!")
