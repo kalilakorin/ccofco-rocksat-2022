9d21d92c85a70d8cfcfe96ff80acb62243500c06
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-04-15 16:00:37 -0600

    Update telemetry_read.py

--- telemetry_read.py
+++ telemetry_read.py
@@ -7,12 +7,13 @@ import serial
 
 ser = serial.Serial(
         port='/dev/ttyUSB0',
-        baudrate = 9600,
+        baudrate=9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=1
 )
+ser.open()
 # def main():
 while 1:
         x=ser.readline()
