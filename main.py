511995089e4db185bc05bb7ac1005ca4ab5fe29e
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-04-29 15:34:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 #!/usr/bin/python
 '''
     This is the main control script for the CC of CO RockSat 2021-2022 payload.
-
     Contributors:
         Jillian Frimml
         Skyler Puckett
         Konstantin Zaremski
-    
+
     --- This product contains software from the previous payload written
         by Andrew Bruckbauer and Konstantin Zaremski for RockSat 2019 - 2021.
-    
+
     Testing:
         The control program can be tested using the '--test' argument. Testing
         mode simulates a shutdown.
-    
+
     Reset:
         The control program can be reset using the '--reset' argument.
         The reset argument clears any persisting save state so that operaiton
         can be tested as if running for the first time.
         $ python control.py --reset
-        Flags can be supplied together (optional):    
+        Flags can be supplied together (optional):
         $ python control.py --reset --test
-    
+
     Functionality:
         This program controls all payload functionality. All individual software
         subsystems for other experiments are integrated as modules that run in
         their own sub processes/threads. Timer events should be listened to within
-        this file and not any of the other sub modules in case changes need to be 
+        this file and not any of the other sub modules in case changes need to be
         made to timing.
 '''
 
@@ -43,7 +42,7 @@ import sensors
 import fram
 import armMotor
 import gopro
-
+import goprotest
 
 # Create a log folder if it does not exist yet
 os.system('mkdir -p ./logs')
@@ -52,7 +51,7 @@ boottime = int(time.time())
 rotatingFileHandler = RotatingFileHandler(
     filename=f'logs/rocksat_payload_{str(boottime)}.log',
     mode='a',
-    maxBytes=20*1024*1024,
+    maxBytes=20 * 1024 * 1024,
     backupCount=2,
     encoding='utf-8',
     delay=0
@@ -62,8 +61,8 @@ logging.basicConfig(
     format='[%(asctime)s.%(msecs)03d][%(module)7s][%(levelname)8s]\t%(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[rotatingFileHandler])
-    
-#formatter = logging.Formatter('[%(asctime)s.%(msecs)03d][%(module)7s][%(levelname)8s]\t%(message)s')
+
+# formatter = logging.Formatter('[%(asctime)s.%(msecs)03d][%(module)7s][%(levelname)8s]\t%(message)s')
 
 logger = logging.getLogger(__name__)
 
@@ -79,10 +78,10 @@ if __name__ == '__main__':
         processQueue = multiprocessing.Queue()
         # Accept command line arguments
 
-        # If no command line arguments are passed the script will assume that it is running in 
+        # If no command line arguments are passed the script will assume that it is running in
         arguments = sys.argv
         runAll = len(arguments) == 1
-        
+
         # Secondary experiment (radiation RAM)
         if ('--fram' in arguments or runAll):
             framExperimentThread = multiprocessing.Process(target=fram.main)
@@ -100,17 +99,22 @@ if __name__ == '__main__':
 
         # gopro recording start
         if ('--gopro' in arguments or runAll):
-                goproThread = multiprocessing.Process(target=gopro.main)
-                goproThread.start()
+            goproThread = multiprocessing.Process(target=gopro.main)
+            goproThread.start()
+
+        # gopro wallops RF testing start
+        if ('--gopro' in arguments or runAll):
+            goprotestThread = multiprocessing.Process(target=goprotest.main)
+            goprotestThread.start()
 
         # auxcam fuctions
         if ('--auxcam' in arguments or runAll):
-                auxcamThread = multiprocessing.Process(target=auxcam.main)
-                auxcamThread.start()
+            auxcamThread = multiprocessing.Process(target=auxcam.main)
+            auxcamThread.start()
 
         # Prim
-        #if framExperimentThread: framExperimentThread.join()
-        #p1.terminate()
+        # if framExperimentThread: framExperimentThread.join()
+        # p1.terminate()
     except KeyboardInterrupt:
-        print ('Caught KeyboardInterrupt exiting')
-        
+        print('Caught KeyboardInterrupt exiting')
+
