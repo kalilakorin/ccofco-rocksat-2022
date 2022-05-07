60e3aa13dda6d044c2fa47c385d0386f92371b8a
Skyler Puckett <67851150+Theskyfalls@users.noreply.github.com>
2022-05-06 18:38:20 -0600

    Update motor_demo.py

--- motor_demo.py
+++ motor_demo.py
@@ -20,7 +20,7 @@ GPIO.setup (forward, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Forward
 GPIO.setup (stop, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # STOP
 GPIO.setup (reverse, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # REVERSE
 
-x=0.55
+x=0.6
 
 while True:
     if GPIO.input(forward):
