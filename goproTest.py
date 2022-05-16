23ce293ce4728f63b68bb3f450f3528cac2cb840
kalilakorin <kalila.korin@gmail.com>
2022-05-16 16:41:55 -0600

    Add GoPro rf test file

---
+++ goproTest.py
@@ -0,0 +1,37 @@
+from open_gopro import GoPro
+
+import gopromain as gopro
+import time
+import subprocess
+import logging
+
+# test address D1:70:A4:FC:21:4F
+# flight address E3:BB:1E:0D:C8:52
+
+print("\nStart in Gopro test...")
+try:
+    logger = logging.getLogger(__name__)
+except:
+    logger = None
+    print('Unable to acquire the global logger object, assuming that gopro.py is being run on its own')
+
+def main():
+    # Configure & initialize the gopro to receive power from GPIO pins
+    logging.info('Initializing GoPro RF test')
+
+    logger.info('Wallops RF test start: ' + str(int(time.time() * 1000)))
+
+    # wait 15 seconds to allow camera to turn on
+    time.sleep(15)
+
+    # start the gopro
+    subprocess.call(f'python3 gopromain.py --verbose -a "E3:BB:1E:0D:C8:52" -c "record start"', shell=True)
+    logger.info('GoPro record started: ' + str(int(time.time() * 1000)))
+    time.sleep(5)
+    subprocess.call(f'python3 gopromain.py --verbose -a "E3:BB:1E:0D:C8:52" -c "record stop"', shell=True)
+    logger.info('GoPro record stopped: ' + str(int(time.time() * 1000)))
+
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
