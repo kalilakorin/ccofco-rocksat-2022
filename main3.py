447dcc6f2bba79be752956cb5bf0f06db1a73490
kalilakorin <kalila.korin@gmail.com>
2022-05-17 19:27:27 -0600

    Update inhibits testing

--- main3.py
+++ main3.py
@@ -88,19 +88,19 @@ def main():
         arguments = sys.argv
         runAll = len(arguments) == 1
 
-        if ('--auxcam' in arguments or runAll):
-            auxcamThread = multiprocessing.Process(target=auxcam.main)
-            auxcamThread.start()
-
-        # Secondary experiment (radiation RAM)
-        if ('--fram' in arguments or runAll):
-            framExperimentThread = multiprocessing.Process(target=fram.main)
-            framExperimentThread.start()
-
-        # Tertiary experiment (sensors)
-        if ('--sensors' in arguments or runAll):
-            sensorThread = multiprocessing.Process(target=sensors.main)
-            sensorThread.start()
+        # if ('--auxcam' in arguments or runAll):
+        #     auxcamThread = multiprocessing.Process(target=auxcam.main)
+        #     auxcamThread.start()
+        #
+        # # Secondary experiment (radiation RAM)
+        # if ('--fram' in arguments or runAll):
+        #     framExperimentThread = multiprocessing.Process(target=fram.main)
+        #     framExperimentThread.start()
+        #
+        # # Tertiary experiment (sensors)
+        # if ('--sensors' in arguments or runAll):
+        #     sensorThread = multiprocessing.Process(target=sensors.main)
+        #     sensorThread.start()
 
         # Normal flight functionality
         te1 = 27  # TE-1
@@ -129,6 +129,9 @@ def main():
         except:
             logger.critical('Failed to initialize GPIO pins and motor hat.\n')
             return
+
+        motor.motor1.throttle = 0
+        motor.motor4.throttle = 0
 
         # inhibit testing
         if GPIO.input(am):
