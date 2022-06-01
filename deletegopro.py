58c477b523f4bd35f433677300383e851104fa9e
kalilakorin <kalila.korin@gmail.com>
2022-05-31 19:12:25 -0600

    Update with directory paths

--- deletegopro.py
+++ deletegopro.py
@@ -1,7 +1,17 @@
+
+
 from goprocam import GoProCamera, constants
 gpCam = GoProCamera.GoPro()
+
+deletepath = '/home/pi/Documents/gopro-ble-py-2'
+homepath = '/home/pi/ccofco-rocksat-2022'
+
+print("Current working directory: {0}".format(cwd))
+os.chdir(deletepath)
+print("Changed to directory: {0}".format(cwd))
+
 
 ## Downloads all of the SD card's contents and then formats the sd card.
 
 # gpCam.downloadAll()
-gpCam.delete("all")
+# gpCam.delete("all")
