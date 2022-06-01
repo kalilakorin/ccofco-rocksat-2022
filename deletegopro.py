a56607e8e8d2b73a6a80a1964cd640eabcff546b
kalilakorin <kalila.korin@gmail.com>
2022-05-31 19:19:35 -0600

    comment out change the change to home directory

--- deletegopro.py
+++ deletegopro.py
@@ -12,9 +12,9 @@ time.sleep(2)
 os.chdir(deletepath)
 print("Changed to directory: {0}".format(os.getcwd()))
 gpCam.delete("all")
-time.sleep(20)
-os.chdir(homepath)
-print("Changed to directory: {0}".format(os.getcwd()))
+# time.sleep(20)
+# os.chdir(homepath)
+# print("Changed to directory: {0}".format(os.getcwd()))
 
 
 ## Downloads all of the SD card's contents and then formats the sd card.
