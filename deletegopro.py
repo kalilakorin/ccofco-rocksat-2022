1da5c6f53d359f225b57e23924ff317d8de4c2e4
kalilakorin <kalila.korin@gmail.com>
2022-05-31 19:25:18 -0600

    comment out change the change to home directory

--- deletegopro.py
+++ deletegopro.py
@@ -1,23 +1,24 @@
-import os
-import time
-
-from goprocam import GoProCamera, constants
-gpCam = GoProCamera.GoPro()
-
-deletepath = '/home/pi/Documents/gopro-py-api-master'
+# import os
+# import time
+#
+# from goprocam import GoProCamera, constants
+# gpCam = GoProCamera.GoPro()
+#
+deletepath = '/home/pi/Documents/gopro-py-api-master/examples'
 homepath = '/home/pi/ccofco-rocksat-2022'
 
 print("Current working directory: {0}".format(os.getcwd()))
 time.sleep(2)
 os.chdir(deletepath)
 print("Changed to directory: {0}".format(os.getcwd()))
-gpCam.delete("all")
-# time.sleep(20)
-# os.chdir(homepath)
-# print("Changed to directory: {0}".format(os.getcwd()))
-
-
-## Downloads all of the SD card's contents and then formats the sd card.
-
-# gpCam.downloadAll()
+subprocess.call(f'python3 dump_sdcard.py', shell=True)
 # gpCam.delete("all")
+# # time.sleep(20)
+# # os.chdir(homepath)
+# # print("Changed to directory: {0}".format(os.getcwd()))
+#
+#
+# ## Downloads all of the SD card's contents and then formats the sd card.
+#
+# # gpCam.downloadAll()
+# # gpCam.delete("all")
