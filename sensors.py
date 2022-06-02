7b1245aef66446e2c9d0350c781decabe25f6d2d
kalilakorin <49327054+kalilakorin@users.noreply.github.com>
2022-06-01 18:58:32 -0600

    Remove import that is not needed

--- sensors.py
+++ sensors.py
@@ -20,7 +20,6 @@ import busio
 # Import sensor modules
 from adafruit_bme280 import basic as adafruit_bme280
 import adafruit_vl53l1x
-import adafruit_vl53l0x #can delete this later if it doesn't work
 import adafruit_adxl34x
 
 ser = serial.Serial(
